69c77a2ea83cd851818a412f5fdd782355938e64
khalidbinibrahim <khalidbinibrahim01@gmail.com>
2024-04-11 15:38:17 +0600

    context api updated

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -3,10 +3,12 @@ import { createContext } from "react";
 export const AuthContext = createContext(null);
 
 const AuthProvider = ({ children }) => {
-    
-    <AuthContext.Provider value={'hello'}>
-        { children }
-    </AuthContext.Provider>
+
+    return (
+        <AuthContext.Provider value={'hello'}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 export default AuthProvider;
